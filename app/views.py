dee6b2bc623002b2f3561acfcc60079d7740fd8d
Hezron-tech <kiprophez@gmail.com>
2022-02-27 12:20:11 +0300

    adding article function

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 
 from flask import render_template
 from app import app
-from app.request import get_news
+from app.request import get_news,article_source,get_category
 
 # Views
 @app.route('/')
@@ -19,3 +19,24 @@ def index():
     title='news app'
     # headlines=headlines
     return render_template('index.html',title=title,business=business_sources,technology=tech_sources,sports=sports_sources)
+
+@app.route('/article/<id>')
+def article(id):
+
+    '''
+    View article page function that returns the various article details page and its data
+    '''
+    # title= 'Articles'
+    articles = article_source(id)
+    return render_template('article.html',articles= articles,id=id )
+
+@app.route('/categories/<cat_name>')
+def category(cat_name):
+    '''
+    function to return the categories.html page and its content
+    '''
+    category = get_category(cat_name)
+    title = f'{cat_name}'
+    cat = cat_name
+
+    return render_template('categories.html',title = title,category = category, cat= cat_name)    
